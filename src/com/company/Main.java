ef3550be854978d5655f207372e887c7e6e9e539
Nastya <realovka@mail.ru>
2020-05-08 10:12:59 +0300

    3 commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,23 +6,22 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Сколько денег у Пети?");
-        Scanner scanner=new Scanner(System.in);
-        int m=scanner.nextInt();
+        Scanner scanner = new Scanner(System.in);
+        int m = scanner.nextInt();
         System.out.println("Сколько стоит мороженое?");
-        Scanner scanner1=new Scanner(System.in);
-        int k=scanner1.nextInt();
-        int i=0; // количество мороженого
+        Scanner scanner1 = new Scanner(System.in);
+        int k = scanner1.nextInt();
+        int i = 0; // количество мороженого
 
-            if (m<k) {
-                System.out.println("У Пети недостаточно денег даже на одно мороженое");
+        if (m < k) {
+            System.out.println("У Пети недостаточно денег даже на одно мороженое");
+        } else {
+            while (m >= k) {
+                m -= k;
+                i++;
             }
-            else{
-                while (m>=k){
-                    m-=k;
-                    i++;
-                }
-                System.out.println("У Пети останется "+m+" рублей и он съест "+i+" мороженых");
-            }
+            System.out.println("У Пети останется " + m + " рублей и он съест " + i + " мороженых");
         }
     }
+}
 
